210cc5d3ed64daf29cb0a630edb29c943b3dd0f2
Aaron Brady <aleebrady@gmail.com>
2020-08-02 05:42:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_supplies[:fall][:Labor_Day] = array 
-holiday_hash
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
