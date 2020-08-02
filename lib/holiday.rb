9ab3c91fcddf32b7e3de968ec76c2b59d814ec6f
Aaron Brady <aleebrady@gmail.com>
2020-08-02 06:06:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,11 +46,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
- holiday_hash.each do |season, holiday|
-   puts "#{season.capitalize}:"
-   holiday.each do |day, supplies|
-     if day.to_s.include?(" ")
-       holiday_string = 
+ 
 end
 
 def all_holidays_with_bbq(holiday_hash)
