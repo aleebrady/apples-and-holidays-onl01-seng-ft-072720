75496e4c9ef101cf0983f86bec8a10e87f9f3676
Aaron Brady <aleebrady@gmail.com>
2020-08-02 05:44:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,11 +29,11 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_supplies[:spring][:memorial_day] << "Grill"
+  holiday_hash[:spring][:memorial_day] << "Grill"
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_supplies[:fall][:Labor_Day] = supply_array 
+  holiday_hash[:fall][:Labor_Day] = supply_array 
   holiday_hash
 end
 
