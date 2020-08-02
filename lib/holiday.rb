4621cb8586decbd1d19a1065e91612d7792399e5
Aaron Brady <aleebrady@gmail.com>
2020-08-02 05:53:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,11 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  
+  winter = []
+  holiday_hash.each do |season, supplies|
+    winter << supplies
+  end
+  winter.flatten 
 end
 
 def all_supplies_in_holidays(holiday_hash)
